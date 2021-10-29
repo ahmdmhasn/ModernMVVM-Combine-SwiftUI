d5c06d44b4fef1798e2e0bc6837fec24b71930d9
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2021-10-29 16:27:44 +0200

    Initial Commit

---
+++ ModernMVVMDemo/ModernMVVMDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernMVVMDemoApp.swift
+//  ModernMVVMDemo
+//
+//  Created by Ahmed M. Hassan on 29/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct ModernMVVMDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
