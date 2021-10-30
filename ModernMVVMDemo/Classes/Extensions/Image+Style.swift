b2142a961ab3dbfa5165a6245a15712f1d466ed3
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2021-10-30 21:12:34 +0200

    Add extension for Image to allow easy access

---
+++ ModernMVVMDemo/Classes/Extensions/Image+Style.swift
@@ -0,0 +1,22 @@
+//
+//  Image+Style.swift
+//  ModernMVVMDemo
+//
+//  Created by Ahmed M. Hassan on 30/10/2021.
+//
+
+import Foundation
+import SwiftUI
+
+/// App images for easy access
+/// 
+extension Image {
+    
+    static var emptyMovie: Image {
+        return Image(systemName: "film.circle")
+    }
+    
+    static var exclamationMark: Image {
+        return Image(systemName: "exclamationmark.triangle.fill")
+    }
+}
